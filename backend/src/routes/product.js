0e1e2f1722e44ecc5a998514519c745257f21225
Ron <ron062394@gmail.com>
2024-02-11 01:16:12 +0800

    added get a single product router

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
     createProduct,
     getAllProducts,
+    getProductById,
 } = require('../controllers/product')
 
 router.post('/', createProduct);
 router.get('/', getAllProducts);
+router.get('/:id', getProductById);
 
 module.exports = router;
