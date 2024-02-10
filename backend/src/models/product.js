e5566a431aae94293d88eb4e8ecc969bceabd366
Ron <ron062394@gmail.com>
2024-02-10 19:59:10 +0800

    Added ratings on Product schema

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -1,20 +1,38 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  image: String,
-  category: String,
+    name: {
+        type: String,
+        required: true
+    },
+    description: { 
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+        image: {
+        type: String,
+        required: true
+    },
+        category: {
+        type: String,
+        required: true
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    totalRatings: {
+        type: Number,
+        default: 0
+    },
+    qtySold: {
+        type: Number,
+        default: 0
+    },
 }, { timestamps: true });
 
 const Product = mongoose.model('Product', productSchema);
