4a7f26278c442147c15ccca46a17f717b6212af0
Ron <ron062394@gmail.com>
2024-02-11 00:18:17 +0800

    added cart schema

---
+++ backend/src/models/cart.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  products: [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    }
+  }]
+}, { timestamps: true });
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
