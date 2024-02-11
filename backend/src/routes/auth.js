f4911ce9e25c5802b64b3848d493019d4e043759
Ron <ron062394@gmail.com>
2024-02-12 00:32:58 +0800

    Added login and register routes

---
+++ backend/src/routes/auth.js
@@ -0,0 +1,12 @@
+//authRoutes.js
+const express = require('express');
+const router = express.Router();
+const {
+    registerUser,
+    loginUser,
+} = require('../controllers/auth');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
