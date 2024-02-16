f0bda0408b2f39c07e2ff90b8cca4409cd62ae74
Ron <ron062394@gmail.com>
2024-02-16 15:23:20 +0800

    Log-in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <div className="dark"></div>
       <Header/>
-      <Home/>
-      <h1>Pages here</h1>
+      <Login/>
     </div>
   );
 }
