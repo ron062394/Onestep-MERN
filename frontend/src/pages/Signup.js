709f7be40af356392ae178ba4e28b610a3630dcd
Ron <ron062394@gmail.com>
2024-02-16 22:37:56 +0800

    Added signup functionality

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,20 +1,87 @@
+import { useState } from 'react';
 import './Signup.css'
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    contactNumber: '',
+    password: '',
+    birthDay: '',
+    country: ''
+  })
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSignup = async () => {
+    try {
+      const response = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="signup-section">
-        <div className="signup-container">
-            <img src="https://freepngimg.com/save/17091-running-shoes-png-clipart/670x363" alt="black and pink shoe" />
-            <div className="signup-card">
-                <span>Sign up an account</span>
-                <input type="text" placeholder="First Name"/>
-                <input type="text" placeholder="Last Name"/>
-                <input type="email" placeholder="Email" />
-                <input type="number" placeholder="Contact Number" />
-                <input type="password" placeholder="Password"/>
-                <button className="signup-btn secondary-btn">Complete Sign-up</button>
-            </div> 
-        </div>
+      <div className="signup-container">
+        <img src="https://freepngimg.com/save/17091-running-shoes-png-clipart/670x363" alt="black and pink shoe" />
+        <div className="signup-card">
+          <span className='signup-text'>Sign up an account</span>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={handleInputChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="contactNumber"
+            placeholder="Contact Number"
+            onChange={handleInputChange}
+          />
+          <input
+            type="date"
+            name="birthDay"
+            placeholder="Birth Day"
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            onChange={handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleInputChange}
+          />
+          <button className="signup-btn secondary-btn" onClick={handleSignup}>Complete Sign-up</button>
+        </div> 
+      </div>
     </div>
   );
 }
