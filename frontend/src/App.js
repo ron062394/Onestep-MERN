0c9f5d8c5c2dafc214e53f56ad97847779040937
Ron <ron062394@gmail.com>
2024-02-18 17:09:03 +0800

    added minor source changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,21 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import AdminLogin from "./pages/admin/AdminLogin";
+import Sidebar from "./components/admin/AdminSidebar";
+import AdminHeader from "./components/admin/AdminHeader";
+import Dashboard from "./pages/admin/Dashboard";
+import AddProduct from "./pages/admin/AddProduct";
 
 function App() {
   return (
     <div className="App">
       <div className="dark"></div>
-      <Header/>
-        <AdminLogin/>
-      <Footer/>
+        <AdminHeader/>
+        <div className="admin-container">
+          <Sidebar/>
+          <Dashboard/>
+        </div>
+
     </div>
   );
 }
