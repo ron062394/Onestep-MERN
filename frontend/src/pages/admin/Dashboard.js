185e80bc393122d46094f36ec6b95ed29cb84d65
Ron <ron062394@gmail.com>
2024-02-18 17:06:02 +0800

    added admin dashboard

---
+++ frontend/src/pages/admin/Dashboard.js
@@ -0,0 +1,12 @@
+import './Dashboard.css'
+import AddProduct from './AddProduct';
+function Dashboard() {
+    return (
+      <div className="dashboard">
+        <AddProduct/>
+      </div>
+    );
+}
+
+export default Dashboard;
+  
