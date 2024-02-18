b3b87386a49b0909ffb7be1e6a61bf01cff48eda
Ron <ron062394@gmail.com>
2024-02-18 17:04:56 +0800

    added admin compnents

---
+++ frontend/src/components/admin/AdminSidebar.js
@@ -0,0 +1,19 @@
+import './AdminSidebar.css'
+
+function Sidebar() {
+    return (
+      <div className="sidebar">
+        <ul>
+            <li>Dashbord</li> 
+            <li>Product Management</li> 
+            <li>Order Management</li> 
+            <li>Customer</li> 
+            <li>Reports</li> 
+
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Sidebar;
+  
