04f9bbf98167eabe40ead6caae029b6e73a98d4b
Ron <ron062394@gmail.com>
2024-02-24 06:05:57 +0800

    Product List Component: Added more styles

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -9,65 +9,16 @@ function ProductList() {
               <span className='new'>NEW</span>
               <span className='product-rate'><img className='product-star' src="https://pngimg.com/d/star_PNG41474.png" alt="" />4.5<span>(20)</span></span>
             </div>
-            <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
-            <div className='product-card-price'>$195.00</div>
-            <div className='product-card-title emphasized'>LOREM IPSILUM</div>
-            <div className='product-card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-          </div>
-          <button className='cart-btn'>ADD TO CART</button>
-        </div>
-        <div className='product-card'>
-          <div className='product-card-content'>
-            <div className='product-uppertext'>
-              <span className='new'>NEW</span>
-              <span className='product-rate'><img className='product-star' src="https://pngimg.com/d/star_PNG41474.png" alt="" />4.5<span>(20)</span></span>
+            <div className='product-img-container'>
+              <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
             </div>
-            <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
             <div className='product-card-price'>$195.00</div>
             <div className='product-card-title emphasized'>LOREM IPSILUM</div>
             <div className='product-card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
           </div>
           <button className='cart-btn'>ADD TO CART</button>
         </div>
-        <div className='product-card'>
-          <div className='product-card-content'>
-            <div className='product-uppertext'>
-              <span className='new'>NEW</span>
-              <span className='product-rate'><img className='product-star' src="https://pngimg.com/d/star_PNG41474.png" alt="" />4.5<span>(20)</span></span>
-            </div>
-            <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
-            <div className='product-card-price'>$195.00</div>
-            <div className='product-card-title emphasized'>LOREM IPSILUM</div>
-            <div className='product-card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-          </div>
-          <button className='cart-btn'>ADD TO CART</button>
-        </div>
-        <div className='product-card'>
-          <div className='product-card-content'>
-            <div className='product-uppertext'>
-              <span className='new'>NEW</span>
-              <span className='product-rate'><img className='product-star' src="https://pngimg.com/d/star_PNG41474.png" alt="" />4.5<span>(20)</span></span>
-            </div>
-            <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
-            <div className='product-card-price'>$195.00</div>
-            <div className='product-card-title emphasized'>LOREM IPSILUM</div>
-            <div className='product-card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-          </div>
-          <button className='cart-btn'>ADD TO CART</button>
-        </div>
-        <div className='product-card'>
-          <div className='product-card-content'>
-            <div className='product-uppertext'>
-              <span className='new'>NEW</span>
-              <span className='product-rate'><img className='product-star' src="https://pngimg.com/d/star_PNG41474.png" alt="" />4.5<span>(20)</span></span>
-            </div>
-            <img className="product-card-img" src="https://clipart-library.com/image_gallery2/Running-Shoes-Transparent.png" alt="" />
-            <div className='product-card-price'>$195.00</div>
-            <div className='product-card-title emphasized'>LOREM IPSILUM</div>
-            <div className='product-card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-          </div>
-          <button className='cart-btn'>ADD TO CART</button>
-        </div>
+
       </div>
     </div>
   );
