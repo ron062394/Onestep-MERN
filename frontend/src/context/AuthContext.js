4baa3b914cd682945254971a7a34c5c4eee76b59
Ron <ron062394@gmail.com>
2024-02-25 14:13:27 +0800

    Added AuthContext for dynamic user data

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -17,6 +17,17 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     });
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user) {
+            dispatch({
+                type: 'LOGIN',
+                payload: user,
+            })
+        }
+    }, []);
+
     console.log('AuthContext state', state);
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
