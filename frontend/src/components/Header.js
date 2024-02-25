70133c74ca9e9f519903df71f781d0cc6bbc6ed3
Ron <ron062394@gmail.com>
2024-02-25 14:11:56 +0800

    Header: Revised navigation. Added working sign-in / signout dynamic interface

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,29 +1,54 @@
-import './Header.css'
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function Header() {
-    return (
-      <header className="header-section">
-        <span className="logo"><span className="yellow-color">O</span>NES<span className="yellow-color">T</span>E<span className="yellow-color">P</span></span>
-        <input className="search-input" type="text" placeholder="Search"/>
-        <nav>
-            <ul className="link-list-container">
-                <li className="selected">
-                  <Link className='no-text-decoration' to="/">Home</Link>
-                </li>
-                <li>About us</li>
-                <li>
-                  <Link className='no-text-decoration' to="/login">Login</Link>
-                </li>
-                <li>|</li>
-                <li>
-                  <Link className='no-text-decoration' to="/register">Signup</Link>
-                </li>
-            </ul>
-        </nav>
-      </header>
-    );
-  }
-  
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <header className="header-section">
+      <span className="logo">
+        <span className="yellow-color">O</span>NES
+        <span className="yellow-color">T</span>E
+        <span className="yellow-color">P</span>
+      </span>
+      <input className="search-input" type="text" placeholder="Search" />
+      <nav>
+        <ul className="link-list-container">
+          <li className="selected">
+            <Link className="no-text-decoration" to="/">
+              Home
+            </Link>
+          </li>
+          <li>About us</li>
+          {user ? (
+            <li>
+              {user.firstName}|<button onClick={handleLogout}>Logout</button>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link className="no-text-decoration" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li>|</li>
+              <li>
+                <Link className="no-text-decoration" to="/register">
+                  Signup
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 export default Header;
-  
