7429cd5368575a499a23e211042f3bff2cf4707a
ron062394 <ron062394@gmail.com>
2024-03-14 19:37:24 +0800

    ProductList: Added card designs

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,3 +1,5 @@
+// ProductList.js
+
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -22,7 +24,7 @@ function ProductList() {
             <div className="product-card-content">
               <div className="product-uppertext">
                 <span className="new">NEW</span>
-                <span className="product-rate">
+                {/* <span className="product-rate">
                   <img
                     className="product-star"
                     src="https://i.imgur.com/XsLLxLD.png"
@@ -32,7 +34,7 @@ function ProductList() {
                     ? (product.totalRatings / product.ratings).toFixed(2)
                     : 0}
                   <span>({product.ratings})</span>
-                </span>
+                </span> */}
               </div>
               <div className="product-img-container">
                 {product.images.length > 0 && (
@@ -43,7 +45,6 @@ function ProductList() {
                   />
                 )}
               </div>
-
               <div className="product-card-price emphasized">
                 ${product.price}
               </div>
@@ -53,15 +54,26 @@ function ProductList() {
               <div className="product-card-description limited-height2">
                 {product.description}
               </div>
+              <div className="product-card-overlay">
+                <Link
+                  to={`/product/${product._id}`}
+                  className="btn-link"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <button className="cart-btn">
+                    <span class="text">View Product</span>
+                    <svg
+                      class="arrow"
+                      viewBox="0 0 448 512"
+                      height="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path>
+                    </svg>
+                  </button>
+                </Link>
+              </div>
             </div>
-            <button className="cart-btn">
-              <Link
-                to={`/product/${product._id}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                ADD TO CART
-              </Link>
-            </button>
           </div>
         ))}
       </div>
