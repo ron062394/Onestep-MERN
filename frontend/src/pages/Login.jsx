10ddfc64236b95908221c777282397bec252d7b4
ron062394 <ron062394@gmail.com>
2024-03-17 14:19:48 +0800

    Added login design and change edge designs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,17 +31,19 @@ function Login() {
       <div className="login-container">
         {/* <img src="https://freepngimg.com/save/17091-running-shoes-png-clipart/670x363" alt="black and pink shoe" /> */}
         <div className="login-card">
-          <span className='login-text'>Login</span>
+          <span className='login-text'>Great to have you back!</span>
           <form onSubmit={handleLogin}> {/* Form element */}
+            <label>Email Address</label>            
             <input
               type="email"
               name="email"
-              placeholder="Email"
+              placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
 
+            <label>Password</label>
             <input
               type="password"
               name="password"
@@ -50,22 +52,28 @@ function Login() {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <button disabled={isLoading} className="button">
-              <span class="text">{isLoading ? 'Logging In...' : 'Login'}</span>
-              <svg
-                class="arrow"
-                viewBox="0 0 448 512"
-                height="1rem"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path>
-              </svg>
-            </button>
+            <div>
+              <button disabled={isLoading} className="button">
+                <span class="text">{isLoading ? 'Logging In...' : 'Login'}</span>
+                <svg
+                  class="arrow"
+                  viewBox="0 0 448 512"
+                  height="1rem"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path>
+                </svg>
+              </button>
+              <span className="signup-link">Don't have an account yet? Click here</span>
+            </div>
           </form>
           {error && <p className="error-message">{error}</p>}
         </div> 
       </div>
     </div>
+
+
+
   );
 }
 
