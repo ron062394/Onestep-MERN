188942a65202af4631ac18b2445ee25928d414b2
ron062394 <ron062394@gmail.com>
2024-03-17 17:17:42 +0800

    Added dynamic button as reusable component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useLogin } from '../hooks/useLogin';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 import Background from '../components/Background';
+import DynamicButton from "../components/DynamicButton"; // Import the Button component
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -57,17 +58,7 @@ function Login() {
               required
             />
             <div>
-              <button disabled={isLoading} className="button">
-                <span class="text">{isLoading ? 'Logging In...' : 'Login'}</span>
-                <svg
-                  class="arrow"
-                  viewBox="0 0 448 512"
-                  height="1rem"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path>
-                </svg>
-              </button>
+              <DynamicButton isLoading={isLoading} text="Login" loadingText="Logging In..." />
               <span className="signup-link">Don't have an account yet? Click here</span>
             </div>
 
