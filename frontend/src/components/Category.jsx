f47a8c1661f64fed4bde7d0f23eaa4b8a191108d
ron062394 <ron062394@gmail.com>
2024-03-23 13:19:06 +0800

    Added category.jsx

---
+++ frontend/src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Category.css'
+function Category() {
+    return(
+        <div>
+            <span>Mens</span>
+            <span>Women</span>
+            <span>Sports</span>
+            <span>Kids</span>
+            <span>Sneakers</span>
+            <span>Running Shoes</span>
+            <span>Accessories</span>
+        </div>
+    )    
+}
+
+export default Category;
