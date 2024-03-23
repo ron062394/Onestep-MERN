3c51eebe8187870f0de1c8b0fe62666888c10da3
ron062394 <ron062394@gmail.com>
2024-03-23 21:26:06 +0800

    Added checkout timeline

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-// import "./Checkout.css"
+import "./Checkout.css"
 
 function Checkout() {
     return (
@@ -8,84 +8,100 @@ function Checkout() {
           <span>Back</span>
         </div>
         <hr />
-        <div>
-          progress bar
+        <div class="timeline">
+          <div class="circle done"></div>
+          <div class="line done"></div>
+          <div class="circle done"></div>
+          <div class="line"></div>
+          <div class="circle"></div>
         </div>
-
-        <div>
+        <div class="delivery-timeline">
           <div>Delivery Address</div>
-          <div>
-            <span>
-              <div>
-                <span>John Doe</span>
-                <span>Edit</span>
-              </div>
-              <div>Address</div>
-              <div>Contact</div>
-            </span>
-          </div>
-          <div>
-            <span>
-              <div>
-                <span>John Doe</span>
-                <span>Edit</span>
-              </div>
-              <div>Address</div>
-              <div>Contact</div>
-            </span>
-          </div>
-          <div>
-            <span>
-              <div>
-                <span>John Doe</span>
-                <span>Edit</span>
-              </div>
-              <div>Address</div>
-              <div>Contact</div>
-            </span>
-          </div>
+          <div>Select Payment</div>
+          <div>Order Complete</div>
         </div>
+        <div className="check-out-container">
+          <div>
+            <div>Delivery Address</div>
+            <div   className="address-container">
+              <div>
+                <span>
+                  <div>
+                    <span>John Doe</span>
+                    <span>Edit</span>
+                  </div>
+                  <div>Address</div>
+                  <div>Contact</div>
+                </span>
+              </div>
+              <div>
+                <span>
+                  <div>
+                    <span>John Doe</span>
+                    <span>Edit</span>
+                  </div>
+                  <div>Address</div>
+                  <div>Contact</div>
+                </span>
+              </div>
+              <div>
+                <span>
+                  <div>
+                    <span>John Doe</span>
+                    <span>Edit</span>
+                  </div>
+                  <div>Address</div>
+                  <div>Contact</div>
+                </span>
+              </div>
 
-        <hr/>
-        <div>SELECT PAYMENT METHOD</div>
-        <div>
-          <span>Credit Card</span>
-          <span>Paypal</span>
-          <span>COD</span>
-        </div>
+            </div>
 
-        <div>Name On Card</div>
-        <input type="text" placeholder="Your Card Number"/>
-        <div>Card Number</div>
-        <input type="number" placeholder="0000-0000-0000-0000"/>
-        <div>
-          <div>Prder Suummary</div>
-          <div>
-            <span>Merchandise Total Quantity:</span>
-            <span>$534.00</span>
+
+
+            <hr/>
+          <div>SELECT PAYMENT METHOD</div>
+            <div>
+              <span>Credit Card</span>
+              <span>Paypal</span>
+              <span>COD</span>
+            </div>
+
+            <div>Name On Card</div>
+            <input type="text" placeholder="Your Card Number"/>
+            <div>Card Number</div>
+            <input type="number" placeholder="0000-0000-0000-0000"/>
           </div>
           <div>
-            <span>Merchandise Total:</span>
-            <span>$534.00</span>
+            <div>Order Suummary</div>
+            <div>
+              <span>Merchandise Total Quantity:</span>
+              <span>$534.00</span>
+            </div>
+            <div>
+              <span>Merchandise Total:</span>
+              <span>$534.00</span>
+            </div>
+            <div>
+              <span>Discount:</span>
+              <span>$35.00</span>
+            </div>
+            <div>
+              <span>Subtotal:</span>
+              <span>$429.00</span>
+            </div>
+            <div>
+              <span>Shipping:</span>
+              <span>$19.00</span>
+            </div>
+            <div>
+              <span>Total Payment:</span>
+              <span>$410.00</span>
+            </div>
+            <div></div>
+            <button>Checkout</button>
           </div>
-          <div>
-            <span>Discount:</span>
-            <span>$35.00</span>
-          </div>
-          <div>
-            <span>Subtotal:</span>
-            <span>$429.00</span>
-          </div>
-          <div>
-            <span>Shipping:</span>
-            <span>$19.00</span>
-          </div>
-          <div>
-            <span>Total Payment:</span>
-            <span>$410.00</span>
-          </div>
-          <div></div>
-          <button>Checkout</button>
+
         </div>
 
       </section>
