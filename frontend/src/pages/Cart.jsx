9b838c145c3010d8b4f0e3f43feb62822656fccb
ron062394 <ron062394@gmail.com>
2024-03-27 17:45:17 +0800

    Added Cart Loading Screen

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,10 +1,12 @@
 import ProductList from "../components/ProductList";
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
+import Loading from "../components/Loading";
 import "./Cart.css";
 
 function Cart() {
   const [cart, setCart] = useState([]); // Initialize cart state as an empty array
+  const [loading, setLoading] = useState(true); // Initialize loading state as true
   const { user } = useAuthContext();
 
   useEffect(() => {
@@ -35,6 +37,8 @@ function Cart() {
         }
       } catch (error) {
         console.error("Error fetching cart data:", error);
+      } finally {
+        setLoading(false); // Set loading state to false after fetching data
       }
     };
 
@@ -49,77 +53,79 @@ function Cart() {
 
   return (
     <div className="cart-section">
-      <div className="cart-section-container">
-        <div className="cart-section-header">
-          <span>Your Cart</span>
-          <span className="text-underlined continue-shopping">
-            Continue Shopping
-          </span>
+      {loading ? ( // Conditionally render loading indicator
+        <div className="loading-container">
+          <Loading />
         </div>
-        <div className="cart-container">
-          <div className="cart-content-header">
-            <span>Description</span>
-            <span>Size</span>
-            <span>Quantity</span>
-            <span>Remove</span>
-            <span>Price</span>
-          </div>
-
-          {/* Check if cart is an array before using map */}
-          {/* Check if cart is an array before using map */}
-          {Array.isArray(cart) &&
-            cart.map((item) => (
-              <div key={item._id} className="cart-content-container">
-                <div>
-                  {item.product &&
-                    item.product.images &&
-                    item.product.images[0] && (
-                      <img
-                        className="cart-product-img"
-                        src={item.product.images[0]} // Access the first image
-                        alt="product-img"
-                      />
-                    )}
-                  {item.product && <span>{item.product.product}</span>}
-                </div>
-                <div>
-                  <span>{item.size}</span> {/* Access the 'size' directly */}
-                </div>
-                <div className="cart-item-qty">
-                  <span className="increament-btn">+</span>
-                  <span>{item.quantity}</span>
-                  <span className="decreament-btn">-</span>
-                </div>
-                <div>
-                  <span className="remove-btn">x</span>
-                </div>
-                <div>
-                  <span>
-                    ${(item.product.price * item.quantity).toFixed(2)}
-                  </span>
-                </div>
-              </div>
-            ))}
-
-          <div className="total-container">
-            {/* Calculate total quantity and price here */}
-            <span>
-              Quantity: {cart.reduce((total, item) => total + item.quantity, 0)}
-            </span>
-            <span>
-              Total:{" "}$
-              {cart
-                .reduce(
-                  (total, item) => total + item.quantity * item.product.price,
-                  0
-                )
-                .toFixed(2)}
+      ) : (
+        <div className="cart-section-container">
+          <div className="cart-section-header">
+            <span>Your Cart</span>
+            <span className="text-underlined continue-shopping">
+              Continue Shopping
             </span>
           </div>
-
-          <button className="secondary-btn">CHECK OUT</button>
+          <div className="cart-container">
+            <div className="cart-content-header">
+              <span>Description</span>
+              <span>Size</span>
+              <span>Quantity</span>
+              <span>Remove</span>
+              <span>Price</span>
+            </div>
+            {/* Check if cart is an array before using map */}
+            {Array.isArray(cart) &&
+              cart.map((item) => (
+                <div key={item._id} className="cart-content-container">
+                  <div>
+                    {item.product &&
+                      item.product.images &&
+                      item.product.images[0] && (
+                        <img
+                          className="cart-product-img"
+                          src={item.product.images[0]} // Access the first image
+                          alt="product-img"
+                        />
+                      )}
+                    {item.product && <span>{item.product.product}</span>}
+                  </div>
+                  <div>
+                    <span>{item.size}</span> {/* Access the 'size' directly */}
+                  </div>
+                  <div className="cart-item-qty">
+                    <span className="increament-btn">+</span>
+                    <span>{item.quantity}</span>
+                    <span className="decreament-btn">-</span>
+                  </div>
+                  <div>
+                    <span className="remove-btn">x</span>
+                  </div>
+                  <div>
+                    <span>
+                      ${(item.product.price * item.quantity).toFixed(2)}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            <div className="total-container">
+              {/* Calculate total quantity and price here */}
+              <span>
+                Quantity: {cart.reduce((total, item) => total + item.quantity, 0)}
+              </span>
+              <span>
+                Total:{" "}$
+                {cart
+                  .reduce(
+                    (total, item) => total + item.quantity * item.product.price,
+                    0
+                  )
+                  .toFixed(2)}
+              </span>
+            </div>
+            <button className="secondary-btn">CHECK OUT</button>
+          </div>
         </div>
-      </div>
+      )}
       <div className="other-section">
         <h3>You might like</h3>
         <ProductList />
