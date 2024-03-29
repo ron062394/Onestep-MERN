1554863ac58c487573af0173cb7b6f9f48080db6
ron062394 <ron062394@gmail.com>
2024-03-29 22:53:10 +0800

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext'; // Import AuthContextProvider
+import { CartProvider } from './context/CartContext'; // Import CartProvider
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <CartProvider> {/* Wrap App with CartProvider */}
+        <App />
+      </CartProvider>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
