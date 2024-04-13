eeeeff9558dea912f3fa52eb31d9ae9fe7e55d81
ron062394 <ron062394@gmail.com>
2024-04-13 15:03:57 +0800

    feat: Added controller for editAddress

--- backend/src/controllers/address.js
+++ backend/src/controllers/address.js
@@ -1,6 +1,6 @@
 const Address = require('../models/address');
-// Controller method to add a new address to a user
 
+// Controller method to add a new address to a user
 const addAddress = async (req, res) => {
     try {
         const { street, city, state, postalCode, country } = req.body;
@@ -23,8 +23,22 @@ const addAddress = async (req, res) => {
       }
 };
 
+const editAddress = async (req, res) => {
+  try {
+    const { street, city, state, postalCode, country } = req.body;
+    const address = await Address.findByIdAndUpdate(req.params.addressId, { street, city, state, postalCode, country }, { new: true });
+    if (!address) {
+      return res.status(404).json({ message: 'Address not found' });
+    }
+    res.json({ message: 'Address updated successfully', address });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error });
+  }
+};
+
 
 module.exports = {
     addAddress,
+    editAddress,
 };
   
